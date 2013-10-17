7fcd06b62020b5b505d6d0dee6a9e8feb3f7f453
Park Hyunwoo <ez@smartstudy.co.kr>
2013-10-17 14:53:28 +0900

    Add custom toolbar configuration via settings

--- django_summernote/views.py
+++ django_summernote/views.py
@@ -1,5 +1,21 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.utils import simplejson
 
 
 def editor(request, id):
-    return render(request, 'django_summernote/editor.html', {'id': id})
+    config = getattr(settings, 'SUMMERNOTE_CONFIG', {
+        'toolbar': [
+            ['style', ['style']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['para', ['ul', 'ol', 'height']],
+            ['insert', ['link', 'picture']],
+            ['help', ['help']],
+        ],
+    })
+    return render(request, 'django_summernote/editor.html', {
+        'id': id,
+        'toolbar': simplejson.dumps(config['toolbar']),
+    })
