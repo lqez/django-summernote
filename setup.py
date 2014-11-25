a5bf1c96ae54eceb22784d7550869e09819b9f17
Park Hyunwoo <ez@smartstudy.co.kr>
2014-11-25 13:07:52 +0900

    Drop Python 2.6 support from pypi categories

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ CLASSIFIERS = [
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.3',
