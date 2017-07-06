4457afa505e617aa9fc4d2c996cddf74e7486af3
Steve <steve@mclenithan.com>
2017-07-06 11:35:49 -0500

    Added a test for the ugettext_lazy __proxy__ value scenario

--- django_summernote/tests.py
+++ django_summernote/tests.py
@@ -52,6 +52,18 @@ class DjangoSummernoteTest(TestCase):
 
         assert 'summernote' in html
 
+    def test_widget_inplace_with_ugettext_lazy(self):
+        from django_summernote.widgets import SummernoteInplaceWidget
+        from django.utils.translation import ugettext_lazy
+
+        widget = SummernoteInplaceWidget()
+        # Force an attribute to be an unevaluated __proxy__ type. This is how django handles string translations.
+        html = widget.render(
+            'foobar', 'lorem ipsum', attrs={'id': 'id_foobar', 'width': ugettext_lazy('100%')}
+        )
+ 
+         assert 'summernote' in html
+
     def test_form(self):
         from django import forms
         from django_summernote.widgets import SummernoteWidget
